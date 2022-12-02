26b638fcfed61b65cacb0a6bb36b2f04e6f92324
Mohith Mallampati <mohith.mallampati@sap.com>
2022-12-02 17:29:31 +0530

    Added landing

---
+++ lib/index.dart
@@ -0,0 +1,11 @@
+// Export pages
+export 'start_page/start_page_widget.dart' show StartPageWidget;
+export 'login/login_widget.dart' show LoginWidget;
+export 'onboarding_page/onboarding_page_widget.dart' show OnboardingPageWidget;
+export 'home_page/home_page_widget.dart' show HomePageWidget;
+export 'join_game/join_game_widget.dart' show JoinGameWidget;
+export 'create_game_step1/create_game_step1_widget.dart'
+    show CreateGameStep1Widget;
+export 'create_game_step2/create_game_step2_widget.dart'
+    show CreateGameStep2Widget;
+export 'game_page/game_page_widget.dart' show GamePageWidget;
