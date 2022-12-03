c5e84364a4ddbf56462234d5257335b4e8aff403
Mohith Mallampati <mohith.mallampati@sap.com>
2022-12-03 14:07:37 +0530

    Made modifications

---
+++ lib/start_page/start_page_widget.dart
@@ -0,0 +1,119 @@
+import '../flutter_flow/flutter_flow_animations.dart';
+import '../flutter_flow/flutter_flow_theme.dart';
+import '../flutter_flow/flutter_flow_util.dart';
+import '../flutter_flow/flutter_flow_widgets.dart';
+import '../login/login_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class StartPageWidget extends StatefulWidget {
+  const StartPageWidget({Key? key}) : super(key: key);
+
+  @override
+  _StartPageWidgetState createState() => _StartPageWidgetState();
+}
+
+class _StartPageWidgetState extends State<StartPageWidget>
+    with TickerProviderStateMixin {
+  final animationsMap = {
+    'containerOnPageLoadAnimation': AnimationInfo(
+      trigger: AnimationTrigger.onPageLoad,
+      effects: [
+        FadeEffect(
+          curve: Curves.easeInOut,
+          delay: 1500.ms,
+          duration: 800.ms,
+          begin: 0,
+          end: 1,
+        ),
+        ScaleEffect(
+          curve: Curves.easeInOut,
+          delay: 1500.ms,
+          duration: 800.ms,
+          begin: 1,
+          end: 1,
+        ),
+      ],
+    ),
+  };
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    setupAnimations(
+      animationsMap.values.where((anim) =>
+          anim.trigger == AnimationTrigger.onActionTrigger ||
+          !anim.applyInitialState),
+      this,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: Color(0xFFF5F5F5),
+      body: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 1,
+            decoration: BoxDecoration(
+              color: Color(0xFFEEEEEE),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: Image.asset(
+                  'assets/images/images.png',
+                ).image,
+              ),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/1-48.jpg',
+                  height: 270,
+                  fit: BoxFit.contain,
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 15, 0, 0),
+                  child: FFButtonWidget(
+                    onPressed: () async {
+                      await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => LoginWidget(),
+                        ),
+                      );
+                    },
+                    text: 'Get Started',
+                    options: FFButtonOptions(
+                      width: 270,
+                      height: 70,
+                      color: FlutterFlowTheme.of(context).customColor2,
+                      textStyle: FlutterFlowTheme.of(context).title1.override(
+                            fontFamily: 'Poppins',
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                      elevation: 4,
+                      borderSide: BorderSide(
+                        color: Colors.transparent,
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ).animateOnPageLoad(animationsMap['containerOnPageLoadAnimation']!),
+        ],
+      ),
+    );
+  }
+}
